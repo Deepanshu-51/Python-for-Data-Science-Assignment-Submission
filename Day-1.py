5e7ddb079a81f4851b4c29bfead9516113eb65e4
Deepanshu <64122103+Deepanshu-51@users.noreply.github.com>
2021-09-20 22:23:54 +0530

    Day-1 Assignment summited

---
+++ Day-1.py
@@ -0,0 +1,5 @@
+principle = float(input("Principle: "))
+rate = float(input("Rate of Intrest: "))
+time = float(input("Time Duration: "))
+simple_intrest = principle * rate * time /100
+print(f"Simplle Interest: {round(simple_intrest,2)}")
